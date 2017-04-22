951348b199d745249f3e9c039d994341f8a1df45
Clement-R <rondeau.clement@gmail.com>
2017-04-22 14:18:15 +0200

    Change project settings to set asset serialization to text mode

---
+++ Assets/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	void Update () {
+	    if(Input.GetAxisRaw("Horizontal") > 0) {
+            Debug.Log(Input.GetAxisRaw("Horizontal"));
+        } else if(Input.GetAxisRaw("Horizontal") < 0) {
+            Debug.Log(Input.GetAxisRaw("Horizontal"));
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0) {
+            Debug.Log(Input.GetAxisRaw("Vertical"));
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0) {
+            Debug.Log(Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
